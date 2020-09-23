92726edd85642f02525fe415edb183ab3a642f90
WY-777 <617028204@qq.com>
2020-09-23 09:53:43 +0800

    学生管理系统第一次提交

---
+++ Untitled-1.py
@@ -0,0 +1,17 @@
+while True:
+    op = input('请输入序号')
+    if op == '1':
+        print('显示所有学生信息')
+    elif op == '2':
+        print('新建学生信息')
+    elif op == '3':
+        print('查询学生信息')
+    elif op == '4':
+        print('修改学生信息')
+    elif op == '5':
+        print('删除学生信息')
+    elif op == '0':
+        print('退出系统')
+        break
+    else:
+        print('')
